59e77bbc0efc93158cbd6c2d6f240a98b5eff814
Robert <Robert@ROBERTSVAIO>
2026-03-30 23:12:11 -0400

    Add project files.

---
+++ SimpleSlide/MainPage.xaml.cs
@@ -0,0 +1,196 @@
+﻿using System;
+using Windows.UI.Popups;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+namespace SimpleSlide
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a <see cref="Frame">.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        private readonly String PauseStr = "Pause";
+        private readonly String PauseTT = "Pause slide show";
+        private readonly String ContiueStr = "Continue";
+        private readonly String ContinueTT = "Continue slide show";
+
+        private readonly String? PickedFolderToken = "PickedFolderToken";
+
+        public Progress<String> FNameProgress;
+        private Player Player;
+
+        // Play speeds
+        private readonly int SlowSpeed = 20 * 1000;
+        private readonly int MediumSpeed = 15 * 1000;
+        private readonly int FastSpeed = 10 * 1000;
+
+        public MainPage()
+        {
+            InitializeComponent();
+
+            FNameProgress = new Progress<String>();
+            Player = new(PickedFolderToken, FNameProgress)
+            {
+                ImagePane = [null, Image1, null], // The XAML image elements
+                DelayBetweenImges = MediumSpeed
+            };
+            FNameProgress.ProgressChanged += FNameChanged;
+
+            // Start the player
+            _ = Player.Play(); // Async operation, Player running on another thread.
+        }
+
+        private void FNameChanged(object? sender, string fileName)
+        {
+            FNameTextBlock.Text = fileName;
+        }
+        private async void SelectFolderBtn_Click(object sender, Windows.UI.Xaml.RoutedEventArgs e)
+        {
+            //Message();
+
+            // Disable Select folders btn
+            ((Button)sender).IsEnabled = false;
+
+            // Send Stop command to player
+            Player.CommandQueue.Enqueue(new PlayerCommand()
+            {
+                Command = PlayerCommand.PlayerCommands.Stop
+            });
+
+            // Let user select folder
+            var folderPicker = new Windows.Storage.Pickers.FolderPicker();
+            folderPicker.SuggestedStartLocation = Windows.Storage.Pickers.PickerLocationId.Desktop;
+            folderPicker.FileTypeFilter.Add("*");
+
+            Windows.Storage.StorageFolder folder = await folderPicker.PickSingleFolderAsync();
+            if (null != folder)
+            {
+                // Application now has read/write access to all contents in the picked folder
+                // (including other sub-folder contents)
+                Windows.Storage.AccessCache.StorageApplicationPermissions.
+                FutureAccessList.AddOrReplace(PickedFolderToken, folder);
+                FNameTextBlock.Text = folder.Path;
+
+                // Change to Pause
+                ContinuePauseBtn.Content = PauseStr;
+                ContinuePauseBtn.IsEnabled = true;
+                SetToolTip(ContinuePauseBtn, PauseTT);
+
+                // Send Play command to player
+                Player.CommandQueue.Enqueue(new PlayerCommand()
+                {
+                    Command = PlayerCommand.PlayerCommands.Play,
+                });
+            }
+            else
+            {
+                // Send Continue command to player
+                Player.CommandQueue.Enqueue(new PlayerCommand()
+                {
+                    Command = PlayerCommand.PlayerCommands.Continue
+                });
+            }
+            ((Button)sender).IsEnabled = true;
+        }
+        private void ContinuePauseBtn_Click(object sender, Windows.UI.Xaml.RoutedEventArgs e)
+        {
+            var CommandQueue = Player.CommandQueue;
+
+            switch (ContinuePauseBtn.Content)
+            {
+                case "Pause":
+                    ContinuePauseBtn.Content = ContiueStr;
+                    SetToolTip(ContinuePauseBtn, ContinueTT);
+
+                    // Send Pause command to player
+                    Player.CommandQueue.Enqueue(new PlayerCommand()
+                    {
+                        Command = PlayerCommand.PlayerCommands.Pause
+                    });
+                    break;
+                default: // Continue
+                    ContinuePauseBtn.Content = PauseStr;
+                    SetToolTip(ContinuePauseBtn, PauseTT);
+
+                    // Send Cotinue command to player
+                    Player.CommandQueue.Enqueue(new PlayerCommand()
+                    {
+                        Command = PlayerCommand.PlayerCommands.Continue
+                    });
+                    break;
+            }
+        }
+        private void SetToolTip(DependencyObject element, String value)
+        {
+            var tt = new ToolTip()
+            {
+                Content = value
+            };
+            ToolTipService.SetToolTip(element, tt);
+        }
+
+        /// <summary>
+        /// A messagebox in UWP land
+        /// </summary>
+        private void Message()
+        {
+            // Create the message dialog and set its content
+            var messageDialog = new MessageDialog("Button pushed.");
+
+            // Add commands and set their callbacks; both buttons use the same callback function instead of inline event handlers
+            messageDialog.Commands.Add(new UICommand(
+                "Try again",
+                new UICommandInvokedHandler(this.CommandInvokedHandler)));
+            messageDialog.Commands.Add(new UICommand(
+                "Close",
+                new UICommandInvokedHandler(this.CommandInvokedHandler)));
+
+            // Set the command that will be invoked by default
+            messageDialog.DefaultCommandIndex = 0;
+
+            // Set the command to be invoked when escape is pressed
+            messageDialog.CancelCommandIndex = 1;
+
+            // Show the message dialog
+            messageDialog.ShowAsync();
+
+        }
+        private void CommandInvokedHandler(IUICommand command)
+        {
+            // Display message showing the label of the command that was invoked
+            //rootPage.NotifyUser("The '" + command.Label + "' command has been selected.",
+            //    NotifyType.StatusMessage);
+        }
+
+        private void SlowSpeed_RB_Click(object sender, RoutedEventArgs e)
+        {
+            // Send ChangeSpeed command to player
+            Player.CommandQueue.Enqueue(new PlayerCommand()
+            {
+                Command = PlayerCommand.PlayerCommands.ChangeSpeed,
+                Value = SlowSpeed
+            });
+        }
+
+        private void MediumSpeed_RB_Click(object sender, RoutedEventArgs e)
+        {
+            // Send ChangeSpeed command to player
+            Player.CommandQueue.Enqueue(new PlayerCommand()
+            {
+                Command = PlayerCommand.PlayerCommands.ChangeSpeed,
+                Value = MediumSpeed
+            });
+        }
+
+        private void FastSpeed_RB_Click(object sender, RoutedEventArgs e)
+        {
+            // Send ChangeSpeed command to player
+            Player.CommandQueue.Enqueue(new PlayerCommand()
+            {
+                Command = PlayerCommand.PlayerCommands.ChangeSpeed,
+                Value = FastSpeed
+            });
+        }
+    }
+}
---
+++ SimpleSlide/Player.cs
@@ -0,0 +1,206 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Windows.Storage;
+using Windows.Storage.Search;
+using Windows.Storage.Streams;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Media.Imaging;
+
+namespace SimpleSlide
+{
+    /// <summary>
+    /// Place one of these on Queue to get commmad to the Player 
+    /// </summary>
+    internal class PlayerCommand
+    {
+        public enum PlayerCommands
+        {
+            Stop,       // Stop the player
+            Play,       // Start new from StartFolder
+            Pause,      // Pause play
+            Continue,   // Contiue play
+            Forward,    // Move on to next item/pic
+            Backward,   // Go to previous item/pic
+            ChangeSpeed // Change speed to this many milliseconds between images (Value)
+        }
+        public PlayerCommands Command { get; set; }
+        public int Value { get; set; } = 0;
+        public PlayerCommand() { }
+    }
+
+    /// <summary>
+    /// One of these is pushed on the Stack, for each folder being traversed
+    /// </summary>
+    internal class FolderState
+    {
+        public int CurrentFolderNum { get; set; } = 0;
+        public IReadOnlyList<StorageFolder>? FolderList { get; set; }
+
+        public FolderState() { }
+    }
+
+    /// <summary>
+    /// Background task doing the media playing
+    /// </summary>
+    internal class Player
+    {
+        public enum PlayerState // What is player doing
+        {
+            DoingNothing,
+            Playing,
+            Paused
+        }
+        public PlayerState CurrentPlayerState { get; set; }
+        public String? PickedFolderToken { get; set; }
+        public IProgress<String> FNameProgress;
+
+        // For passing commands from UI to Player
+        public ConcurrentQueue<PlayerCommand> CommandQueue { get; private set; }
+        private IReadOnlyList<StorageFile>? CurrentFolderFileList { get; set; } // All the files in a folder
+        private int NextFileNum;
+        private Stack<FolderState> FoldersStack = new();
+        public int DelayBetweenImges { get; set; }
+
+        public Image?[] ImagePane = new Image[3];
+
+        /// <summary>
+        /// Contstructor
+        /// </summary>
+        /// <param name="fNameProgress"></param>
+        public Player(String pickedFolderToken, Progress<String> fNameProgress)
+        {
+            PickedFolderToken = pickedFolderToken;
+            FNameProgress = fNameProgress;
+            CommandQueue = new();
+            CurrentPlayerState = PlayerState.DoingNothing; // Doing Nothing;
+        }
+
+        /// <summary>
+        /// Play media and respond to commands
+        /// </summary>
+        /// <returns></returns>
+        /// <remarks>
+        /// Log running background task
+        /// Plays each media file in a folder then begis to descend into the subfolders.
+        /// Recursive algorithm, but it is implemeted with a stack.
+        /// </remarks>
+        public async Task Play()
+        {
+            PlayerCommand? playerCommand;
+
+            int i = 0;
+            while (true)
+            {
+                // Anythig on the command queue?
+                if (CommandQueue.TryDequeue(out playerCommand))
+                {
+                    switch (playerCommand.Command)
+                    {
+                        case PlayerCommand.PlayerCommands.Stop:
+                            CurrentPlayerState = PlayerState.DoingNothing;
+                            break;
+                        case PlayerCommand.PlayerCommands.Pause:
+                            CurrentPlayerState = PlayerState.Paused;
+                            break;
+                        case PlayerCommand.PlayerCommands.Continue:
+                            CurrentPlayerState = PlayerState.Playing;
+                            break;
+                        case PlayerCommand.PlayerCommands.Play:
+                            CurrentPlayerState = PlayerState.Playing;
+                            PrepForFolder();
+                            i = 0; // Start new
+                            break;
+                        case PlayerCommand.PlayerCommands.ChangeSpeed:
+                            DelayBetweenImges = playerCommand.Value; // Miliseconds
+                            break;
+                        default:
+                            break;
+                    }
+                }
+
+                if (PlayerState.Playing == CurrentPlayerState)
+                {
+                    /*
+                     * if (there is a next media file in CurrentFolderFileList)
+                     *  play it
+                     * else
+                     *  descend into next folder in this folder
+                     *          PrepFolder();
+                     * 
+                     */
+                    if (null != CurrentFolderFileList) // List is ready/available
+                    {
+                        // Round and round
+                        if (i >= CurrentFolderFileList.Count)
+                            i = 0;
+
+                        Image? imagePane = ImagePane[1];
+
+                        StorageFile sF = CurrentFolderFileList[i];
+
+                        using (IRandomAccessStream fileStream = await sF.OpenAsync(Windows.Storage.FileAccessMode.Read))
+                        {
+                            // Set the image source to the selected bitmap 
+                            BitmapImage bitmapImage = new BitmapImage();
+                            bitmapImage.DecodePixelWidth = (int)imagePane.Width; //match the target Image.Width, not shown
+                            await bitmapImage.SetSourceAsync(fileStream);
+                            imagePane?.Source = bitmapImage;
+                        }
+
+                        FNameProgress.Report(i.ToString() + " " + sF.Name);
+                        i++;
+                    }
+                }
+
+                await Task.Delay(DelayBetweenImges);
+            }
+        }
+
+        /// <summary>
+        /// Prepare to begin playig from the taget directory named in commandString
+        /// </summary>
+        /// <param name="storageFolder">Pass in a StorageFolder is available. Otherwie will get it
+        /// from the PickedFolderToken of the FutureAccessList.</param>
+        /// <remarks>
+        /// These types
+        /// .jpeg, .jpg, .png, .bmp, gif, .tiff, .ico, .svg
+        /// </remarks>
+        private async void PrepForFolder(Windows.Storage.StorageFolder? storageFolder=null)
+        {
+            var folder = (Windows.Storage.StorageFolder) await Windows.Storage.AccessCache.StorageApplicationPermissions.
+                FutureAccessList.GetItemAsync(PickedFolderToken);
+
+            // Retrieve list of any media files
+            // There is a bug in QueryOptios class that causes a cast exception when a List is passed
+            // for the fie type filter list. There is mention of it in various forums. Only work-around
+            // found is what is impemented here...
+            var fileTypeFilterList = new List<String>() { ".jpeg", ".jpg", ".png", ".bmp", ".gif", ".tiff", ".ico", ".svg" };
+            //var queryOptions = new QueryOptions(CommonFileQuery.OrderByName, fileTypeFilterList);
+            var queryOptions = new QueryOptions();
+            foreach(String fileType in fileTypeFilterList)
+                queryOptions.FileTypeFilter.Add(fileType);
+            var query = folder.CreateFileQueryWithOptions(queryOptions);
+            CurrentFolderFileList = null;
+            CurrentFolderFileList = await query.GetFilesAsync();   // Files in current folder
+
+            //            foreach (StorageFile file in fileList)
+            //            {
+            //                outputText.Append(file.Name + "\n");
+            //            }
+
+            // Get list of all the folders in this folder. Push that list
+            // onto the folder stack. Gonna do it this way instead of using true recursion.
+            IReadOnlyList<StorageFolder>? currentFolderFolderList;      // All the folders in a folder
+            currentFolderFolderList = await folder.GetFoldersAsync();   // Folders in current folder
+
+            // Make a FolderState to push onto FoldersStack
+            FolderState folderState = new()
+            {
+                FolderList = currentFolderFolderList
+            };
+            FoldersStack.Push(folderState);
+        }
+    }
+}
